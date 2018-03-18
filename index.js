57e46cb696947d9e1cba6c31d7ee1a548a741d8f
Szikszai Gusztáv <guszti5@hotmail.com>
2018-03-18 08:53:40 +0100

    Deploy 2018-03-18 08:53:40

--- index.js
+++ index.js
@@ -2475,7 +2475,7 @@ class $Main extends React.PureComponent {
     return item.name == this.page
     }), this.pages)))
 
-    return [React.createElement($Layout, {  }, [content]), React.createElement("noscript", {}, [React.createElement("iframe", {
+    return [React.createElement("script", {}, [`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TJJ8DCX');`]), React.createElement($Layout, {  }, [content]), React.createElement("noscript", {}, [React.createElement("iframe", {
       src: `https://www.googletagmanager.com/ns.html?id=GTM-TJJ8DCX`,
       height: `0`,
       width: `0`,
